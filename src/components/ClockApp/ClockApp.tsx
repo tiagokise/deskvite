ddd0d598b6b4a104217bfb122c7e969f849c5264
tiago <tiago@grupoeureka.com.br>
2023-07-03 21:40:56 -0300

    CLOCL

--- src/components/ClockApp/ClockApp.tsx
+++ src/components/ClockApp/ClockApp.tsx
@@ -1,5 +1,4 @@
 'use client'
-
 import Clock from 'react-clock';
 import 'react-clock/dist/Clock.css';
 import { useEffect, useMemo, useState } from 'react';
@@ -11,9 +10,9 @@ export function ClockApp({ clockOpen, setClockOpen}: any) {
   const windowWidth =  useMemo(() => window && window.innerWidth, [window.innerWidth])
   const [clockX, setClockX] = useState<any>((windowWidth / 2) - (204 / 2))
   const [clockY, setClockY] = useState<any>(20)
-  
   const [clockProps, setClockProps] = useState<any>({x: setClockX, y: clockY, width: '204px', height: '204px'})
   const [value, setValue] = useState(new Date());
+  
   useEffect(() => {
     const interval = setInterval(() => setValue(new Date()), 1000);
 
@@ -42,7 +41,6 @@ export function ClockApp({ clockOpen, setClockOpen}: any) {
         setClockProps({x: d.x, y: d.y})
       } }
       enableResizing={false}
-      // enableResizing={{ top:false, right:true, bottom:true, left:false, topRight:false, bottomRight:true, bottomLeft:false, topLeft:false }}
       bounds="parent"
       style={{zIndex: 99}}
       {...clockProps}
@@ -52,7 +50,6 @@ export function ClockApp({ clockOpen, setClockOpen}: any) {
           <S.CloseButton aria-label="close" onClick={() => setClockOpen(!clockOpen)} >
             <HighlightOffTwoToneIcon />
           </S.CloseButton>
-          {/* <iframe src="https://tiagok-dev.netlify.app/" frameBorder="0" width="1024" height="100%" ></iframe> */}
           <Clock value={value} />
         </S.ClockWrapper>
       </S.ClockApp>
