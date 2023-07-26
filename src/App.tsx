c5bd0aa4cf6a75e7974d3f8726ba7553da3fe8e6
tiago <tiago@grupoeureka.com.br>
2023-07-26 20:11:01 -0300

    d

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
-import React, { useState } from "react"
 import { Routes, Route } from "react-router-dom";
 
 import Home from './containers/Home/Home'
-// @ts-ignore
-// import ImageGallery from './components/ImageGallery/ImageGallery.jsx'
 import { Gallery } from "./components/Gallery/Gallery";
-// import './styles/globals.css'
 
 
 
